97e47a46443c74034735d1be9892d0fa0415649b
0kkq <103496290+0kkq@users.noreply.github.com>
2022-04-12 12:44:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from info import *
 from rdod import *
 from btns import * 
-server = Flask(__name__)
+#server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start(m):
@@ -18,17 +18,4 @@ def rdod(m):
 	reply(m)
 
 
-
-
-@server.route(f"/{took}", methods=["POST"])
-def redirect_message():
-    json_string = request.get_data().decode("utf-8")
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
-   
- 
-if __name__ == "__main__":
-    bot.remove_webhook()
-    bot.set_webhook(url="https://telemero.herokuapp.com/"+str(took))
-    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+bot.infinity_polling()
