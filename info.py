df289976228403a9fbc5ba0dd916b1cd803b745e
0kkq <103496290+0kkq@users.noreply.github.com>
2022-04-13 10:19:55 +0300

    Update info.py

--- info.py
+++ info.py
@@ -9,4 +9,4 @@ from flask import Flask, request
 Admin = 1818416834
 took = '5234559529:AAEHj9OoEX5gJr3DjqyICohTB1VpjAAqbQI'
 bot = telebot.TeleBot(took)
-#APP_URL = "https://telemero.herokuapp.com/" + took
+APP_URL = "https://telemero.herokuapp.com/" + took
