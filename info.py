ea028e8ab3c66c526309a4e7540d339ce647c52a
0kkq <103496290+0kkq@users.noreply.github.com>
2022-04-13 10:49:39 +0300

    Update info.py

--- info.py
+++ info.py
@@ -8,6 +8,7 @@ from telebot.types import Message
 
 Admin = 1818416834
 
-bot = telebot.TeleBot(took)
+
 BOT_TOKEN = "5234559529:AAFN-RtwcISXlPlYpXBAT6_mqIuaHy18UJg"
 APP_URL = "https://telemero.herokuapp.com/" + BOT_TOKEN
+bot = telebot.TeleBot(BOT_TOKEN)
