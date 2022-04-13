577d3a1b7581c03287a10282b7e1adab756344f1
0kkq <103496290+0kkq@users.noreply.github.com>
2022-04-13 10:53:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import logging
 from info import *
 from flask import Flask, request
 
-bot = telebot.TeleBot(BOT_TOKEN)
 server = Flask(__name__)
 logger = telebot.logger
 logger.setLevel(logging.DEBUG)
